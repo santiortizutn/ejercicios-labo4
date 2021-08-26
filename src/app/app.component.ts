89a9c5c3baa6fbfb83f24f45df4d0814e7a81bc3
santiortizutn <santyno09@gmail.com>
2021-08-26 20:58:44 -0300

    Primer commit. Deploy en heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio';
+  title = 'Clase 1';
+  edadUno:number = 0;
+  edadDos:number = 0;
+  prom:number = 0;
+  sum:number = 0;
+
+
+  suma(){
+    this.sum = this.edadUno + this.edadDos;
+  }
+
+  promedio(){
+    this.prom = (this.edadUno + this.edadDos) / 2;
+  }
+
+  calcular(){
+    this.suma();
+    this.promedio();
+  }
+
+  limpiar(){
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.prom = 0;
+    this.sum = 0;
+  }
 }
